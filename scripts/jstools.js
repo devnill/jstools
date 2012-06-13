536bfd74db627ba196310753e17c83e6bcb8ce8d
Dan Singer <devnill@gmail.com>
2012-06-12 18:29:05 -0600

    it's alive!

--- scripts/jstools.js
+++ scripts/jstools.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	var hue=1;
 	var starting_hue=Math.random()*360
-	var hue_increment=31; //because prime numbers are cool.
-	var default_luminosity=1;
-	var default_saturation=1;
+	var hue_increment=19; //because prime numbers are cool. --keeps an even distubution of colors
+	var default_luminosity=.5;	
+	var default_saturation=1; 
 	var decay=10;
 	traverse();
 	
@@ -15,7 +15,7 @@ $(document).ready(function(){
 				console.log("current child is:");
 				console.log(this);
 				var current_region=$(this);
-				var current_hue=(starting_hue+(depth*hue_increment))%360;
+				var current_hue=Math.floor((starting_hue+(depth*hue_increment))%360);
 				setBG(current_region,current_hue,depth,function(){
 					traverse($(current_region),depth);
 				});				
@@ -36,9 +36,13 @@ $(document).ready(function(){
 			}
 		}
 		function setBG(region,color,depth,callback){
-		
-			//$(region).css({"backgroundColor":"hsla("+starting_hue+","+default_saturation*(decay/(decay+depth))+","+default_luminosity*(decay/(decay+depth))+",1)"});
-			$(region).css({"backgroundColor":color});
+			
+			saturation=100*default_saturation*(decay/(decay+depth));
+			luminosity=-100*(default_luminosity*(decay/(decay+depth))-1);
+			console.log("setting hue "+color+", saturation "+saturation+", luminosity "+luminosity+" in:");
+			console.log(region);
+			$(region).css({"backgroundColor":"hsla("+color+","+saturation+"%,"+luminosity+"%,1)"});
+			
 			if(callback){
 				callback();
 			}
